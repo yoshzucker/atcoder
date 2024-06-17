2b21b99e670dbd9ef374dbb31af2e1c38da09a56
yoshzucker <yoshzucker@gmail.com>
2024-06-17 13:03:02 +0900

    fix: abc330/b

--- contest/abc330/abc330_b/b.lisp
+++ contest/abc330/abc330_b/b.lisp
@@ -3,11 +3,6 @@
        (r (read))
        (ai (loop repeat n collect (read))))
   (loop for a in ai
-        append (loop for x from l to r
-                     collect (max a (abs (- x a)) for y from l to r
-                                unless (<= (abs (- x a)) (abs (- y a)))
-                                  do (return nil)
-                                finally (return t))
-                       collect x)
+        collect (min (max a l) r)
           into lst
         finally (format t "~{~a ~}" lst)))
