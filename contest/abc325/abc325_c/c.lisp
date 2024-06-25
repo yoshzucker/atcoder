5a3ee13952ac4f04109d61e81791cbbd7fdcea73
yoshzucker <yoshzucker@gmail.com>
2024-06-25 21:49:54 +0900

    feat: abc325/c. work in progress. TLE

--- contest/abc325/abc325_c/c.lisp
+++ contest/abc325/abc325_c/c.lisp
@@ -3,12 +3,13 @@
        (swh (make-array (list h w)
                         :initial-contents
                         (loop repeat h collect (read-line)))))
-  (loop for i below h with stack and visited do
+  (loop for i below h with stack and visited and connected-component = 0 do
     (loop for j below w do
       (when (and (char= (aref swh i j) #\#)
                  (not (find (list i j) visited :test #'equal))) 
         (push (list i j) stack)
         (push (list i j) visited)
+        (incf connected-component)
         (loop while stack for p = (pop stack)
               for f = (first p) and s = (second p) do
                 (loop for l from (max 0 (1- f)) to (min (1+ f) (1- h)) do
@@ -17,5 +18,5 @@
                                (not (find (list l m) visited :test #'equal)))
                       (push (list l m) visited)
                       (push (list l m) stack)))))))
-        finally (return visited)))
+        finally (print connected-component)))
 
