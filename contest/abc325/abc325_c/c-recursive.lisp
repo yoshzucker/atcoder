7e1a99e57031db70ece591bf7b7ba9deb1f3843e
yoshzucker <yoshzucker@gmail.com>
2024-06-27 21:10:37 +0900

    refactor: abc325/c

--- contest/abc325/abc325_c/c-recursive.lisp
+++ contest/abc325/abc325_c/c-recursive.lisp
@@ -13,24 +13,22 @@
 
 (let* ((h (read))
        (w (read))
-       (swh (make-array (list h w)
-                        :initial-contents
-                        (loop repeat h collect (read-line))))
-       (visited (make-array (list h w) :initial-element nil)))
-  (labels ((around (x y)
-             (loop for i from (max 0 (1- x)) to (min (1+ x) (1- h))
-                   append (loop for j from (max 0 (1- y)) to (min (1+ y) (1- w))
-                                when (char= (aref swh i j) #\#)
-                                  collect (list i j))))
-           (dfs (x y)
-             (setf (aref visited x y) t)
-             (loop for (i j) in (around x y)
-                   do (unless (aref visited i j) (dfs i j)))))
-    (loop for i below h
-          sum (loop for j below w
-                    when (and (char= (aref swh i j) #\#)
-                              (not (aref visited i j)))
-                      do (dfs i j) and count it)
-            into it
+       (s (apply #'concatenate 'string (loop repeat h collect (read-line))))
+       (visited (make-array (* h w) :initial-element nil)))
+  (labels ((around (n)
+             (let ((l (floor n w))
+                   (m (mod n w)))
+               (loop for i from (max 0 (1- l)) to (min (1+ l) (1- h))
+                     append (loop for j from (max 0 (1- m)) to (min (1+ m) (1- w))
+                                  for n = (+ (* i w) j)
+                                  when (and (char= (elt s n) #\#)
+                                            (not (elt visited n)))
+                                    collect n))))
+           (dfs (n)
+             (setf (elt visited n) t)
+             (mapcar #'dfs (around n))))
+    (loop for n below (* h w)
+          when (and (char= (elt s n) #\#)
+                    (not (elt visited n)))
+            do (dfs n) and count it into it
           finally (print it))))
-
