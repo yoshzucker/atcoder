cee76e11853296a7d4cf6dc2e1503a7a0387a822
yoshzucker <yoshzucker@gmail.com>
2024-07-02 07:36:52 +0900

    feat: abc329/c

--- contest/abc329/abc329_c/c.lisp
+++ contest/abc329/abc329_c/c.lisp
@@ -1,8 +1,12 @@
 (let ((n (read))
       (s (coerce (read-line) 'list)))
-  (loop for (c . r) on s with count = 0 and alst
-        do (when (char= c (first r))
-             (incf count)
-             (let ((p (assoc c alst)))
-               (setf (cdr p) (max (cdr p) count))))
-        finally ))
+  (loop for (c . r) on s with count = 1 and alst
+        do (if (and r (char= c (first r)))
+               (incf count)
+               (setf count 1))
+           (let ((p (assoc c alst)))
+             (if p
+                 (setf (cdr p) (max (cdr p) count))
+                 (setf alst (acons c count alst))))
+        finally (print (reduce #'+ alst :key #'cdr))))
+  
