207a8254e0ecd7cc8ebf04cecf4819b1c9d08cd3
yoshzucker <yoshzucker@gmail.com>
2024-07-03 08:22:43 +0900

    perf: abc331/c

--- contest/abc331/abc331_c/c.lisp
+++ contest/abc331/abc331_c/c.lisp
@@ -1,12 +1,11 @@
 (let* ((n (read))
        (an (sort (loop for i below n collect (cons i (read))) #'> :key #'cdr)))
-  (loop for ((nil . a) . ((i . r))) on an with sum = 0 and stock = 0
+  (loop for ((nil . a) . ((i . r))) on an
+        with sum = 0 and stock = 0 and vec = (make-array n :initial-element 0)
         when r
           do (if (/= a r)
                  (setf sum (+ sum a stock)
                        stock 0)
                  (incf stock a))
-          and collect (cons i sum) into it
-        finally (setf it (acons (car (first an)) 0 it))
-                (loop for i below n
-                      do (format t "~a " (cdr (assoc i it))))))
+             (setf (elt vec i) sum)
+        finally (loop for i below n do (format t "~a " (elt vec i)))))
