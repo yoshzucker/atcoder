f3dd2bba1393473bfb933d818b1fe615969b43fe
yoshzucker <yoshzucker@gmail.com>
2024-07-06 14:29:32 +0900

    feat: abc337/c

---
+++ contest/abc337/abc337_c/c.lisp
@@ -0,0 +1,8 @@
+(let* ((n (read))
+       (prevn (loop repeat n collect (read)))
+       (nextn (make-array (1+ n) :initial-element nil)))
+  (loop for next from 1 to n for prev in prevn
+        do (setf (elt nextn (max 0 prev)) next))
+  (loop repeat n for next = (elt nextn 0) then (elt nextn next)
+        do (format t "~a " next)))
+  
