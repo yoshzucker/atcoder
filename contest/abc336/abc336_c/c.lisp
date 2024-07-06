f36de7225a154703a0fe80af49069a767cfbab92
yoshzucker <yoshzucker@gmail.com>
2024-07-06 10:46:03 +0900

    refactor: abc336/c

--- contest/abc336/abc336_c/c.lisp
+++ contest/abc336/abc336_c/c.lisp
@@ -1,9 +1,5 @@
-(defun radix (dec radix)
-  (do ((x dec (floor x radix))
-       (lst nil))
-      ((zerop x) (or lst '(0)))
-    (push (mod x radix) lst)))
-
 (let ((n (read)))
-  (let ((quintal (radix (1- n) 5)))
-    (princ (map 'string #'(lambda (x) (digit-char (* x 2))) quintal))))
+  (loop for i from 4 downto 1
+        with str = (write-to-string (1- n) :base 5)
+        do (setf str (substitute (digit-char (* i 2)) (digit-char i) str))
+        finally (princ str)))
