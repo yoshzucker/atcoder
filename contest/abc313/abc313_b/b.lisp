e8b64765e8b87b37c3f67e68bcc678981524a1c8
yoshzucker <yoshzucker@gmail.com>
2024-07-09 08:35:46 +0900

    feat: abc313/b. work in progress

---
+++ contest/abc313/abc313_b/b.lisp
@@ -0,0 +1,7 @@
+(let* ((n (read))
+       (m (read))
+       (abm (loop repeat m collect (list (read) (read))))
+       (visited (make-array n :initial-element nil)))
+  (loop for i below n
+        do (loop while ))
+  )
