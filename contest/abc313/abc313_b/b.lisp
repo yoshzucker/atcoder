fc225d0b3a96ece277099024d78c304933ced7c0
yoshzucker <yoshzucker@gmail.com>
2024-07-09 12:57:01 +0900

    refactor: abc313/b

--- contest/abc313/abc313_b/b.lisp
+++ contest/abc313/abc313_b/b.lisp
@@ -1,14 +1,8 @@
 (let* ((n (read))
        (m (read))
-       (abm (loop repeat m collect (list (1- (read)) (1- (read)))))
-       (loser (make-array n :initial-element nil)))
-  (loop for i below n
-        do (loop with stack = (list i) while stack
-                 do (let ((x (pop stack)))
-                      (loop for (a b) in abm
-                            do (when (and (= a x) (not (elt loser b)))
-                                 (push b stack)
-                                 (setf (elt loser b) t)))))
+       (abm (loop repeat m collect (list (1- (read)) (1- (read))))))
+  (loop for (nil b) in abm with loser = (make-array n :initial-element nil)
+        do (setf (elt loser b) t)
         finally (print (if (= (count nil loser) 1)
                            (1+ (position nil loser))
                            -1))))
