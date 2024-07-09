9c35a386c4449f252c223bda739535a93ef9a27c
yoshzucker <yoshzucker@gmail.com>
2024-07-09 12:51:50 +0900

    feat: abc313/b

--- contest/abc313/abc313_b/b.lisp
+++ contest/abc313/abc313_b/b.lisp
@@ -1,7 +1,14 @@
 (let* ((n (read))
        (m (read))
-       (abm (loop repeat m collect (list (read) (read))))
-       (visited (make-array n :initial-element nil)))
+       (abm (loop repeat m collect (list (1- (read)) (1- (read)))))
+       (loser (make-array n :initial-element nil)))
   (loop for i below n
-        do (loop while ))
-  )
+        do (loop with stack = (list i) while stack
+                 do (let ((x (pop stack)))
+                      (loop for (a b) in abm
+                            do (when (and (= a x) (not (elt loser b)))
+                                 (push b stack)
+                                 (setf (elt loser b) t)))))
+        finally (print (if (= (count nil loser) 1)
+                           (1+ (position nil loser))
+                           -1))))
