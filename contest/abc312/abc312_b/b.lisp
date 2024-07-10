466c1ec29f91f51adfea44c45722e8697dce1e6e
yoshzucker <yoshzucker@gmail.com>
2024-07-11 08:57:40 +0900

    feat: abc312/b

--- contest/abc312/abc312_b/b.lisp
+++ contest/abc312/abc312_b/b.lisp
@@ -8,14 +8,13 @@
                   "-----.###"
                   "-----.###")))
   (defun tak-code-p (strs)
-    (loop for str in strs for tak-str in tak-code
+    (loop named outer for str in strs for tak-str in tak-code
           do (loop for c across str for tak-c across tak-str
-                   do (format t "~a ~a~%" tak-c c)
                    do (unless (or (char= tak-c #\# c)
                                   (char= tak-c #\. c)
                                   (char= tak-c #\-))
-                        (return nil))
-                   finally (return t)))))
+                        (return-from outer nil)))
+          finally (return-from outer t))))
 
 (let* ((n (read))
        (m (read))
@@ -24,4 +23,4 @@
         do (loop for j to (- m 9)
                  for strs = (mapcar #'(lambda (x) (subseq x j (+ j 9))) strsm)
                  when (tak-code-p strs)
-                   do (format t "~a ~a~%" i j))))
+                   do (format t "~a ~a~%" (1+ i) (1+ j)))))
