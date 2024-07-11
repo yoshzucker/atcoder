3039cf3e2040b368abee345a6e2b0ed0a4ac1ed4
yoshzucker <yoshzucker@gmail.com>
2024-07-12 08:26:41 +0900

    refactor: abc311/a

--- contest/abc311/abc311_a/a.lisp
+++ contest/abc311/abc311_a/a.lisp
@@ -1,8 +1,5 @@
 (let ((n (read))
       (s (read-line)))
-  (loop for chr across s with a and b and c for i from 1
-        do (when (char= chr #\A) (setf a t))
-           (when (char= chr #\B) (setf b t))
-           (when (char= chr #\C) (setf c t))
-        until (and a b c)
-        finally (print i)))
+  (print (1+ (max (position #\A s)
+                  (position #\B s)
+                  (position #\C s)))))
