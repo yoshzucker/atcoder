5676110a514cb5f151b8ce70c416f45b98b9a98b
yoshzucker <yoshzucker@gmail.com>
2024-07-17 08:31:41 +0900

    feat: abc304/a

--- contest/abc304/abc304_a/a.lisp
+++ contest/abc304/abc304_a/a.lisp
@@ -1,10 +1,12 @@
-(defun read-str ()
+(defun read-list (&rest functions)
   (loop with str = (read-line) for p = (position #\Space str)
-        collect (subseq str 0 p)
+        for fn = (or (pop functions) fn)
+        collect (funcall (or fn #'identity) (subseq str 0 p))
         while p
         do (setf str (subseq str (1+ p)))))
 
-
 (let* ((n (read))
-       (san (loop repeat n
-                  collect 
+       (san (loop repeat n collect (read-list nil #'parse-integer))))
+  (loop for i below n
+        with s = (position (reduce #'min san :key #'second) san :key #'second)
+        do (format t "~a~%" (first (elt san (mod (+ s i) n))))))
