46b77b9b59221154bceaef7cdb20195e20b85933
yoshzucker <yoshzucker@gmail.com>
2024-07-24 12:48:38 +0900

    feat: abc285/b

--- contest/abc286/abc286_b/b.lisp
+++ contest/abc286/abc286_b/b.lisp
@@ -1,4 +1,22 @@
+(defun type-object (obj)
+  (cond ((listp obj) 'list)
+        ((stringp obj) 'string)
+        ((vectorp obj) 'vector)
+        (t 'unkonwn)))
+
+(defun substitute+ (new old sequence)
+  (let* ((start (search old sequence))
+         (end (+ start (length old))))
+    (concatenate (type-object sequence)
+                 (subseq sequence 0 start)
+                 new
+                 (subseq sequence end))))
+
+(defun substitute+-all (new old sequence)
+  (loop while (search old sequence)
+        do (setf sequence (substitute+ new old sequence))
+        finally (return sequence)))
+
 (let ((n (read))
-      (s (coerce (read-line) 'list)))
-  (loop for (c . r) on s)
-  )
+      (s (read-line)))
+  (princ (substitute+-all "nya" "na" s)))
