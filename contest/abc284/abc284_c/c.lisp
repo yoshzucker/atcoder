806b6a479c51cfa9f1be4132981605b059a029aa
yoshzucker <yoshzucker@gmail.com>
2024-08-14 17:29:16 +0900

    feat: abc284/c

--- contest/abc284/abc284_c/c.lisp
+++ contest/abc284/abc284_c/c.lisp
@@ -1,20 +1,56 @@
+(let (parent rank)
+  (defun uf-init (size)
+    (setf rank (make-array size :initial-element 0))
+    (setf parent (make-array size
+                             :initial-contents
+                             (loop for i below size collect i))))
+
+  (defun uf-root (x &key (test #'=))
+    (labels ((root (x acc)
+               (let ((p (elt parent x)))
+                 (cond ((funcall test p x)
+                        (mapcar #'(lambda (a) (setf (elt parent a) x)) acc)
+                        x)
+                       (t
+                        (root p (cons x acc)))))))
+      (root x nil)))
+
+  ;; (defun uf-root (x &key (test #'=))
+  ;;   (let ((p (elt parent x)))
+  ;;     (if (= p x)
+  ;;         x
+  ;;         (setf (elt parent x) (uf-root p)))))
+
+  (defun uf-samep (x y &key (test #'=))
+    (funcall test (uf-root x :test test) (uf-root y :test test)))
+
+  (defun uf-unite (x y &key (test #'=))
+    (let ((x (uf-root x :test test))
+          (y (uf-root y :test test)))
+      (unless (funcall test x y)
+        (let ((rx (elt rank x))
+              (ry (elt rank y)))
+          (cond ((< rx ry)
+                 (setf (elt parent x) y))
+                (t
+                 (if (= rx ry)
+                     (incf (elt rank x)))
+                 (setf (elt parent y) x)))))))
+
+  (defun uf-ranks ()
+    rank)
+
+  (defun uf-roots ()
+    (loop for i below (length parent) do (uf-root i) finally (return parent)))
+
+  (defun uf-force (list)
+    (setf parent (coerce list 'vector))))
+
 (let* ((n (read))
        (m (read))
-       (uvm (loop repeat m collect (cons (1- (read)) (1- (read)))))
-       (adj (make-array n :initial-element nil)))
+       (uvm (loop repeat m collect (cons (1- (read)) (1- (read))))))
   (loop for (u . v) in uvm
-        do (push v (elt adj u))
-           (push u (elt adj v)))
-  (loop for i below n
-        with visited = (make-array n :initial-element nil)
-        with stack
-        unless (elt visited i)
-          count t into cnt
-          and do (setf (elt visited i) t)
-                 (push i stack)
-                 (loop while stack for node = (pop stack)
-                       do (loop for adjacent in (elt adj node)
-                                unless (elt visited adjacent)
-                                  do (setf (elt visited adjacent) t)
-                                     (push adjacent stack)))
-        finally (print cnt)))
+          initially (uf-init n)
+        do (uf-unite u v))
+  (print (length (remove-duplicates (uf-roots)))))
+
