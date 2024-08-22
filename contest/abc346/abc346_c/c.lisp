b9c056b35d1fb088ba260a7f1c309be4b1a3b6da
yoshzucker <yoshzucker@gmail.com>
2024-08-22 20:56:02 +0900

    feat: abc346/c

--- contest/abc346/abc346_c/c.lisp
+++ contest/abc346/abc346_c/c.lisp
@@ -1,9 +1,4 @@
 (let* ((n (read))
        (k (read))
-       (an (sort (remove-duplicates (loop repeat n collect (read))) #'<)))
-  (loop for i from 1 to k with a = (pop an)
-        unless (= i a)
-          sum i into s
-        when (and an (= i a))
-          do (setf a (pop an))
-        finally (print s)))
+       (an (remove-duplicates (loop repeat n collect (read)))))
+  (print (- (/ (* k (1+ k)) 2) (loop for a in an when (<= a k) sum a))))
