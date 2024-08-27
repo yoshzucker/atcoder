4b7c2919f876562aa8ef294d7a272b331dbbf36f
yoshzucker <yoshzucker@gmail.com>
2024-08-28 08:03:42 +0900

    refactor: abc351/c

--- contest/abc351/abc351_c/c.lisp
+++ contest/abc351/abc351_c/c.lisp
@@ -2,8 +2,7 @@
        (an (loop repeat n collect (read))))
   (loop with lst = (list (pop an)) for a in an
         do (push a lst)
-           (loop while (and (second lst)
-                            (= (first lst) (second lst)))
+           (loop while (eql (first lst) (second lst))
                  do (pop lst)
                     (push (1+ (pop lst)) lst))
         finally (print (length lst))))
