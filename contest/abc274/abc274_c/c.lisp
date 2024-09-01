42f3c65f790f2e588337befee052db8f9a5ec5b9
yoshzucker <yoshzucker@gmail.com>
2024-09-01 15:47:00 +0900

    feat: abc274/c

--- contest/abc274/abc274_c/c.lisp
+++ contest/abc274/abc274_c/c.lisp
@@ -1,10 +1,11 @@
 (let* ((n (read))
        (an (loop repeat n collect (read)))
-       (parent (make-array (+ (* 2 n) 2) :initial-element 0))
-       (answer (make-array (+ (* 2 n) 2) :initial-element nil)))
-  (loop for a in an for i from 0
-        do (setf (elt parent (* 2 (1+ i))) a
-                 (elt parent (1+ (* 2 (1+ i)))) a))
-  (loop for k below (1+ (* 2 n))
-        do (setf (elt answer k) (1+ (elt answer (elt parent k)))))
-  )
+       (par (make-array (* 2 (1+ n)) :initial-element 0))
+       (gen (make-array (* 2 (1+ n)) :initial-element 0)))
+  (loop for i from 1 for a in an
+        do (setf (elt par (* 2 i)) a)
+           (setf (elt par (1+ (* 2 i))) a))
+  (loop for i from 2 below (* 2 (1+ n))
+        do (setf (elt gen i) (1+ (elt gen (elt par i)))))
+  (loop for i from 1 below (* 2 (1+ n))
+        do (format t "~a~%" (elt gen i))))
