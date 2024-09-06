b62f776e417d68f10bf401f57244bd53b540361e
yoshzucker <yoshzucker@gmail.com>
2024-09-06 10:11:40 +0900

    refactor: abc288/c

--- contest/abc288/abc288_c/c.lisp
+++ contest/abc288/abc288_c/c.lisp
@@ -1,41 +1,37 @@
-(let (parent-or-size)
-  (defun make-union-find (size &key (test #'=))
-    (cons (make-array size :initial-element -1) test))
+(defun make-union-find (size &key (test #'=))
+  "return data structure for union by size."
+   (cons (make-array size :initial-element -1) test))
 
-  (defun root-union (x union-table)
-    (destructuring-bind (parent-or-size . test) union-table
-      (if (minusp (elt parent-or-size x))
-          x
-          (setf (elt parent-or-size x)
-                (root-union (elt parent-or-size x) union-table)))))
+(defgeneric (setf parent-union) (new x union-table))
 
-  (defun size-union (x union-table)
-    (destructuring-bind (parent-or-size . test) union-table
-      (- (elt parent-or-size (root-union x union-table)))))
+(defmethod parent-union (x union-table)
+  (elt (car union-table) x))
 
-  (defun samep-union (x y union-table)
-    (destructuring-bind (parent-or-size . test) union-table
-      (funcall test (root-union x union-table) (root-union y union-table))))
+(defmethod (setf parent-union) (new x union-table)
+  (setf (elt (car union-table) x) new))
 
-  (defun unite-union (x y union-table)
-    (destructuring-bind (parent-or-size . test) union-table
-      (let ((x (root-union x union-table))
-            (y (root-union y union-table)))
-        (unless (funcall test x y)
-          (if (< (elt parent-or-size y) (elt parent-or-size x))
-              (rotatef x y))
-          (incf (elt parent-or-size x) (elt parent-or-size y))
-          (setf (elt parent-or-size y) x)))))
+(defun root-union (x union-table)
+  (if (minusp (parent-union x union-table))
+      x
+      (setf (parent-union x union-table)
+            (root-union (parent-union x union-table) union-table))))
 
-  (defun uf-parents ()
-    parent-or-size)
-  
-  (defun uf-roots ()
-    (loop for i below (length parent-or-size) do (root-union i) finally (return parent-or-size)))
+(defun size-union (x union-table)
+  (- (parent-union (root-union x union-table) union-table)))
 
-  (defun uf-force (list)
-    (setf parent-or-size (coerce list 'vector))))
+(defun samep-union (x y union-table)
+  (destructuring-bind (parent-or-size . test) union-table
+    (funcall test (root-union x union-table) (root-union y union-table))))
 
+(defun unite-union (x y union-table)
+  (destructuring-bind (parent-or-size . test) union-table
+    (let ((x (root-union x union-table))
+          (y (root-union y union-table)))
+      (unless (funcall test x y)
+        (if (< (parent-union y union-table) (parent-union x union-table))
+            (rotatef x y))
+        (incf (parent-union x union-table) (parent-union y union-table))
+        (setf (parent-union y union-table) x)))))
 
 (let* ((n (read))
        (m (read))
