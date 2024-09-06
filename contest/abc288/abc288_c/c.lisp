902df10de981172912b5d366729c8c2b57442cf0
yoshzucker <yoshzucker@gmail.com>
2024-09-06 09:21:39 +0900

    refactor: abc288/c

--- contest/abc288/abc288_c/c.lisp
+++ contest/abc288/abc288_c/c.lisp
@@ -1,33 +1,37 @@
 (let (parent-or-size)
-  (defun uf-init (size)
-    (setf parent-or-size (make-array size :initial-element -1)))
+  (defun make-union-find (size &key (test #'=))
+    (cons (make-array size :initial-element -1) test))
 
-  (defun uf-root (x &key (test #'=))
-    (if (minusp (elt parent-or-size x))
-        x
-        (setf (elt parent-or-size x)
-              (uf-root (elt parent-or-size x) :test test))))
+  (defun root-union (x union-table)
+    (destructuring-bind (parent-or-size . test) union-table
+      (if (minusp (elt parent-or-size x))
+          x
+          (setf (elt parent-or-size x)
+                (root-union (elt parent-or-size x) union-table)))))
 
-  (defun uf-size (x &key (test #'=))
-    (- (elt parent-or-size (uf-root x :test test))))
+  (defun size-union (x union-table)
+    (destructuring-bind (parent-or-size . test) union-table
+      (- (elt parent-or-size (root-union x union-table)))))
 
-  (defun uf-samep (x y &key (test #'=))
-    (funcall test (uf-root x :test test) (uf-root y :test test)))
+  (defun samep-union (x y union-table)
+    (destructuring-bind (parent-or-size . test) union-table
+      (funcall test (root-union x union-table) (root-union y union-table))))
 
-  (defun uf-unite (x y &key (test #'=))
-    (let ((x (uf-root x :test test))
-          (y (uf-root y :test test)))
-      (unless (funcall test x y)
-        (if (< (elt parent-or-size y) (elt parent-or-size x))
-            (rotatef x y))
-        (incf (elt parent-or-size x) (elt parent-or-size y))
-        (setf (elt parent-or-size y) x))))
+  (defun unite-union (x y union-table)
+    (destructuring-bind (parent-or-size . test) union-table
+      (let ((x (root-union x union-table))
+            (y (root-union y union-table)))
+        (unless (funcall test x y)
+          (if (< (elt parent-or-size y) (elt parent-or-size x))
+              (rotatef x y))
+          (incf (elt parent-or-size x) (elt parent-or-size y))
+          (setf (elt parent-or-size y) x)))))
 
   (defun uf-parents ()
     parent-or-size)
   
   (defun uf-roots ()
-    (loop for i below (length parent-or-size) do (uf-root i) finally (return parent-or-size)))
+    (loop for i below (length parent-or-size) do (root-union i) finally (return parent-or-size)))
 
   (defun uf-force (list)
     (setf parent-or-size (coerce list 'vector))))
@@ -36,10 +40,9 @@
 (let* ((n (read))
        (m (read))
        (abm (loop repeat m collect (cons (1- (read)) (1- (read))))))
-  (uf-init n)
-  (loop for (a . b) in abm
-        unless (uf-samep a b)
-          do (uf-unite a b)
+  (loop for (a . b) in abm with union = (make-union-find n)
+        unless (samep-union a b union)
+          do (unite-union a b union)
         else
           count t into cnt
         finally (print cnt)))
