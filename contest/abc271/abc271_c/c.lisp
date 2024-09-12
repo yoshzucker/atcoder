fc822666a798c0ad3a8b20e24adcef94355cd95f
yoshzucker <yoshzucker@gmail.com>
2024-09-12 21:07:58 +0900

    feat: abc271/c. work in progress

--- contest/abc271/abc271_c/c.lisp
+++ contest/abc271/abc271_c/c.lisp
@@ -1,7 +1,30 @@
+(defclass queue ()
+  ((list :initform nil)
+   (tail :initform nil)))
+
+(defmethod print-object ((queue queue) stream)
+  (print-unreadable-object (queue stream :type t)
+    (with-slots (list tail) queue
+      (cond ((nthcdr 4 list)
+             (format stream "(~{~S ~}... ~S)"
+                     (subseq list 0 3) (first tail)))
+            (t
+             (format stream "~:S" list))))))
+
+(defmethod dequeue ((queue queue))
+  (with-slots (list) queue
+    (pop list)))
+
+(defmethod enqueue (new-item (queue queue))
+  (with-slots (list tail) queue
+    (let ((new-tail (list new-item)))
+      (if (null list)
+          (setf list new-tail)
+          (setf (cdr tail) new-tail))
+      (setf tail new-tail)))
+  queue)
+
+
 (let* ((n (read))
-       (an (coerce (sort (loop repeat n collect (1- (read))) #'<) 'vector)))
-  (loop for i from 0 with head = 0 and tail = (1- n) while (< head tail)
-        do (if (= i (elt an head))
-               (incf head)
-               (decf tail 2))
-        finally (print i)))
+       (an (coerce (sort (loop repeat n collect (1- (read))) #'<) 'vector))
+       ()))
